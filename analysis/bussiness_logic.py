54da5968bee5212d22fc85ec4c5437edbc66f6c9
Gelei Chen <gelei@aisense.com>
2017-11-21 09:17:21 -0500

    update

--- analysis/bussiness_logic.py
+++ analysis/bussiness_logic.py
@@ -24,6 +24,16 @@ def update_y_array(X,y,timestamp,amount):
 
     return y
 
+def update_y_daily_array(X,y,timestamp,amount):
+    target_index = 0
+    for i in range(len(X)):
+        x_time = X[i]
+        if timestamp < x_time:
+            target_index = i
+            break
+    y[target_index] += amount
+    return y
+
 def main_business_logic():
     txs = find_exchange_txs()
 
@@ -52,6 +62,11 @@ def main_business_logic():
     # build all traxe Y: deposit_amount, withdraw_amount
     deposit_trace_y = [0] * len(X)
     withdraw_trace_y = [0] * len(X)
+    exchange_remain_amount_y = [0] * len(X)
+
+    # Daily Stat
+    deposit_daily_trace_y = [0] * len(X)
+    withdraw_daily_trace_y = [0] * len(X)
 
     for holder in txs:
         holder_txs = txs[holder]
@@ -60,9 +75,19 @@ def main_business_logic():
             [timestamp,from_a,tx_type,to_a,amount] = tx
             if tx_type == in_type:
                 deposit_trace_y = update_y_array(X,deposit_trace_y,timestamp,amount)
+                deposit_daily_trace_y = update_y_daily_array(X,deposit_daily_trace_y,timestamp,amount)
             else:
                 withdraw_trace_y = update_y_array(X,withdraw_trace_y,timestamp,amount)
+                withdraw_daily_trace_y = update_y_daily_array(X,withdraw_daily_trace_y,timestamp,amount)
+
+    for i in range(0,len(X)):
+        exchange_remain_amount_y[i] = deposit_trace_y[i] - withdraw_trace_y[i]
 
     deposit_trace = {"x":X,"y":deposit_trace_y,"name":"Exchange Deposit Amount"}
     withdraw_trace = {"x":X,"y":withdraw_trace_y,"name":"Exchange Withdraw Amount"}
-    plot_using_plotly("RDN Top Investor Analysis",[deposit_trace,withdraw_trace])
+    exchange_remain_amount_trace = {"x":X,"y":exchange_remain_amount_y,"name":"Exchange Remain Amount"}
+    plot_using_plotly("Total RDN Exchange Analysis (Binance,Etherdelta)",[deposit_trace,withdraw_trace,exchange_remain_amount_trace])
+
+    deposit_trace = {"x":X,"y":deposit_daily_trace_y,"name":"Exchange Deposit Amount"}
+    withdraw_trace = {"x":X,"y":withdraw_daily_trace_y,"name":"Exchange Withdraw Amount"}
+    plot_using_plotly("Daily RDN Exchange Analysis (Binance,Etherdelta)",[deposit_trace,withdraw_trace])
