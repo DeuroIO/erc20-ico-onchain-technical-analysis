5da8a011be6dd90c3861aa00efb89189b0aba3e2
Gelei Chen <gelei@aisense.com>
2017-11-21 00:40:13 -0500

    be able to generate version 1 graph

--- analysis/bussiness_logic.py
+++ analysis/bussiness_logic.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.insert(0,'..')
-from data.whale_data import find_whale_account_token_tx
+from data.whale_data import find_whale_account_token_tx,exchnage_accounts,raiden_team_address
 from data.html_helper import check_if_address_name_exists
 from db.amazon_db import check_for_address_name,put_item
 from data.whale_eth_tx_data import *
@@ -10,6 +10,32 @@ from plot.plotly_helper import plot_using_plotly
 holding_account = "holding_account"
 deposit_account = 'deposit_account'
 withdraw_account = "withdraw_account"
+
+in_type = "IN"
+out_type = "OUT"
+
+all_acc_types = dict()
+for acc in exchnage_accounts:
+    all_acc_types[acc] = exchange_type
+
+# 保存最早的交易时间
+from datetime import datetime
+from datetime import timedelta
+the_earliest_tx_time = datetime.now()
+
+
+def update_y_array(X,y,timestamp,amount):
+    target_index = 0
+    for i in range(len(X)):
+        x_time = X[i]
+        if timestamp < x_time:
+            target_index = i
+            break
+
+    for i in range(target_index,len(y)):
+        y[i] += amount
+
+    return y
 
 def perform_bfs_on_accounts(out_txs,top_holder_type,acc,m_type='OUT'):
     print("\t"+m_type)
@@ -29,14 +55,28 @@ def perform_bfs_on_accounts(out_txs,top_holder_type,acc,m_type='OUT'):
         print("\t\t{}".format(investor_type))
         if investor_type == exchange_type:
             top_holder_type[acc] = deposit_account if m_type == "OUT" else withdraw_account
+        all_acc_types[out] = investor_type
+
+    if acc not in top_holder_type:
+        top_holder_type[acc] = holding_account
+
     return top_holder_type
 
 def main_business_logic():
     txs = find_whale_account_token_tx()
     top_holder_type = dict()
 
+    # 找到最早的交易时间
+    global the_earliest_tx_time
+
     for acc in txs:
         print(acc)
+
+        acc_txs = txs[acc]
+        for sub_tx in acc_txs:
+            tx_date = sub_tx[0]
+            if tx_date < the_earliest_tx_time: the_earliest_tx_time = tx_date
+
         scan_name = check_if_address_name_exists(acc)
         if scan_name != "":
             print("{}".format(scan_name))
@@ -66,3 +106,50 @@ def main_business_logic():
 
     for acc in top_holder_type:
         print("{} {}".format(acc,top_holder_type[acc]))
+
+    print("------------------------------------------")
+    for acc in all_acc_types:
+        print("{} {}".format(acc,all_acc_types[acc]))
+
+    # build X time array
+    the_earliest_tx_time = the_earliest_tx_time.replace(minute=0, second=0)
+    current_time = datetime.now().replace(minute=0, second=0)
+    tmp_time = the_earliest_tx_time
+    X = []
+
+    while tmp_time < current_time:
+        X.append(tmp_time)
+        tmp_time += timedelta(hours=1)
+    print(len(X))
+
+    # build all traxe Y: holding_amount, deposit_amount, withdraw_amount
+    amount_trace_y = [0] * len(X)
+    deposit_trace_y = [0] * len(X)
+    withdraw_trace_y = [0] * len(X)
+
+    for holder in txs:
+        holder_type = top_holder_type[holder]
+        holder_txs = txs[holder]
+        print("{} {}".format(holder,holder_type))
+
+        for tx in holder_txs:
+            [timestamp,from_a,tx_type,to_a,amount] = tx
+            if holder_type == holding_account:
+                if tx_type == in_type:
+                    amount_trace_y = update_y_array(X,amount_trace_y,timestamp,amount)
+                else:
+                    amount_trace_y = update_y_array(X,amount_trace_y,timestamp,-amount)
+            elif holder_type == deposit_account:
+                if tx_type == out_type and all_acc_types[to_a] == exchange_type:
+                    deposit_trace_y = update_y_array(X,deposit_trace_y,timestamp,amount)
+            elif holder_type == withdraw_account:
+                if tx_type == in_type and all_acc_types[from_a] == exchange_type:
+                    withdraw_trace_y = update_y_array(X,withdraw_trace_y,timestamp,amount)
+            else:
+                print("ERROR!")
+                assert(False)
+
+    amount_trace = {"x":X,"y":amount_trace_y,"name":"Holding Amount"}
+    deposit_trace = {"x":X,"y":deposit_trace_y,"name":"Exchange Deposit Amount"}
+    withdraw_trace = {"x":X,"y":withdraw_trace_y,"name":"Exchange Withdraw Amount"}
+    plot_using_plotly("RDN Top Investor Analysis",[amount_trace,deposit_trace,withdraw_trace])
--- data/whale_data.py
+++ data/whale_data.py
@@ -6,6 +6,10 @@ Watch_addr = "0x255aa6df07540cb5d3d297f0d0d4d84cb52bc8e6"    #Radien Network
 #Whale operation: etherdelta,binance,liqui.io
 exchnage_accounts = ['0x8d12a197cb00d4747a1fe03395095ce2a5cc6819','0x3f5ce5fbfe3e9af3971dd833d26ba9b5c936f0be','0x8271b2e8cbe29396e9563229030c89679b9470db']
 
+
+# Escape raiden_multi_sig_wallet
+raiden_team_address = "0x00c7122633a4ef0bc72f7d02456ee2b11e97561e"
+
 def find_interstering_accounts():
     # Find accounts
     txs = find_tx_given_token_contract_address(Watch_addr,1,1)
@@ -20,8 +24,8 @@ def find_whale_account_token_tx():
     for account in all_arr:
         counter += 1
         print("{}/{} {}".format(counter,len(all_arr),account))
-        if account in exchnage_accounts:
-            acc_features[account] = []
+        if account == raiden_team_address or account in exchnage_accounts:
+            continue
         else:
             acc_features[account] = find_whale_txs(Watch_addr,account)
     return acc_features
