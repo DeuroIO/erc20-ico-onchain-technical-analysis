a8817cfa12398150dbcbc25ca77fb4663b077e1d
Gelei Chen <gelei@aisense.com>
2017-11-24 10:54:18 -0500

    add SNT

--- main.py
+++ main.py
@@ -17,6 +17,13 @@ exchange_accounts = ['0xfbb1b73c4f0bda4f67dca266ce6ef42f520fbb98','0x1151314c646
 # SNT bot accounts. escape
 escape_accounts = []
 
+symbol = 'CVC'
+Watch_addr = "0x41e5560054824ea6b0732e656e3ad64e20e94e45"    #SNT
+# Bittrex, Bitfinex, Binance, Poloniex,liqui.io
+exchange_accounts = ['0xfbb1b73c4f0bda4f67dca266ce6ef42f520fbb98','0x1151314c646ce4e0efd76d1af4760ae66a9fe30f','0x3f5ce5fbfe3e9af3971dd833d26ba9b5c936f0be','0x5E575279bf9f4acf0A130c186861454247394C06']
+# SNT bot accounts. escape
+escape_accounts = []
+
 wd.Watch_addr = Watch_addr
 wd.exchnage_accounts = exchange_accounts
 
