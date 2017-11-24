83a6b0e9a3afe4272385cb874757f5029d399f0f
Gelei Chen <gelei@aisense.com>
2017-11-23 23:44:19 -0500

    add SNT

--- main.py
+++ main.py
@@ -10,6 +10,13 @@ exchange_accounts = ['0xfbb1b73c4f0bda4f67dca266ce6ef42f520fbb98','0x1151314c646
 # OMG bot accounts. escape
 escape_accounts = ['0x36b01066b7fa4a0fdb2968ea0256c848e9135674']
 
+symbol = 'SNT'
+Watch_addr = "0x744d70fdbe2ba4cf95131626614a1763df805b9e"    #SNT
+# Bittrex, Bitfinex, Binance, Poloniex,liqui.io
+exchange_accounts = ['0xfbb1b73c4f0bda4f67dca266ce6ef42f520fbb98','0x1151314c646ce4e0efd76d1af4760ae66a9fe30f','0x3f5ce5fbfe3e9af3971dd833d26ba9b5c936f0be','0x5E575279bf9f4acf0A130c186861454247394C06']
+# SNT bot accounts. escape
+escape_accounts = []
+
 wd.Watch_addr = Watch_addr
 wd.exchnage_accounts = exchange_accounts
 
