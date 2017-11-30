2b71c97a6e23cb71f1c0659c8ade6580b9430dae
Gelei Chen <gelei@aisense.com>
2017-11-30 14:30:35 -0500

    test

--- analysis/bussiness_logic.py
+++ analysis/bussiness_logic.py
@@ -110,7 +110,7 @@ def main_business_logic(symbol,escape_accounts,coinmarketcap_symbol):
     top_50_holding_values = find_top_50_over_time_helper(acc_holding_values_dict)
     top_50_token_moving_average_trace = calculate_top_50_token_moving_average(top_50_holding_values)
     top_50_list_and_token_amount_change_trace = calculate_top_50_list_and_token_amount_change(top_50_holding_values)
-    plot_top_50_list = plot_using_plotly("Top 30 List and their token amount",[top_50_list_and_token_amount_change_trace,price_trace])
+    plot_top_50_list = plot_using_plotly("Top 30 List and their token amount",top_50_list_and_token_amount_change_trace.append(price_trace))
     print(plot_top_50_list_using_plotly)
     # first_plot = plot_using_plotly("Total {} Exchange Analysis (Bittrex, Bitfinex, Binance, Poloniex,liqui.io, Etherdelta, huobi.pro, CEX.com)".format(symbol),[deposit_trace,withdraw_trace,exchange_remain_amount_trace,holding_amount_trace,price_trace,volume_trace])
 
