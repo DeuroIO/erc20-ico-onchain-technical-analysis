33f2ba774ec16d67df3b7eb861ccdca107ee63f0
Gelei Chen <gelei@aisense.com>
2018-01-06 20:55:37 -0800

    test

--- analysis/holder_tracking.py
+++ analysis/holder_tracking.py
@@ -8,9 +8,9 @@ MINIMUM_TOKEN_AMOUNT_REQUIRED = 0.0
 
 def track_holder_number_over_time(acc_holding_values_dict):
     holder_number_over_time = []
-    for timestmap in acc_holding_values_dict:
+    for timestamp in acc_holding_values_dict:
         number = 1
-        t_dict = acc_holding_values_dict[timestmap]
+        t_dict = acc_holding_values_dict[timestamp]
         print(timestamp)
         for acc in t_dict:
             if t_dict[acc] > MINIMUM_TOKEN_AMOUNT_REQUIRED:
